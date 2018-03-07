164ae7dae02a5e72107771d9a9df890830de1975
姜峰 <jiang.f@Ctrip.com>
2018-03-07 10:48:24 +0800

    '更新indexDB'

--- src/Root.js
+++ src/Root.js
@@ -1,9 +1,80 @@
+// https://github.com/jfengsky/javascript/blob/master/indexedDB/demo.js
+
 import React, { Component } from 'react'
 
 export default class Root extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      dblist: []
+    }
+  }
+
+  componentDidMount() {
+    let db
+    var request = indexedDB.open('books', 1);
+
+    request.onupgradeneeded = function (e) {
+      // console.log('upgrading...');
+      // var db = request.result;
+      // var store = db.createObjectStore("books", { keyPath: "isbn" });
+      // var titleIndex = store.createIndex("by_title", "title", { unique: true });
+      // var authorIndex = store.createIndex("by_author", "author")
+    }
+
+    request.onsuccess = function (e) {
+      console.log('success');
+      db = e.target.result;
+    }
+
+    request.onerror = function (e) {
+      console.log('error');
+      console.dir(e);
+    }
+
+    document.getElementById('addDB').addEventListener('click', function () {
+      console.log('add store');
+      var tx = db.transaction("books", "readwrite");
+      var store = tx.objectStore("books");
+      // 添加 store
+      store.put({ title: 'aaa', author: 'bbb', isbn: 'ccc' });
+      tx.complete = function () {
+        console.log('push complete');
+      }
+    })
+
+
+  }
+
   render() {
     return (
       <div className="container-fluid">
+        <nav className="navbar navbar-expand-lg navbar-light" style={{ background: '#e3f2fd' }}>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Features</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Pricing</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">About</a>
+              </li>
+            </ul>
+
+            <button className="btn btn-primary my-2 my-sm-0" type="submit">添加珠子</button>
+
+          </div>
+        </nav>
+
         <table className="table">
           <tbody>
             <tr>
@@ -50,24 +121,14 @@ export default class Root extends Component {
                 </div>
               </td>
             </tr>
-
-            <tr>
-              <th scope="row"></th>
-              <td>
-                <button type="button" className="btn btn-dark">添加珠子</button>
-              </td>
-              <td>
-                <button type="button" className="btn btn-dark">添加珠子</button>
-              </td>
-              <td>
-                <button type="button" className="btn btn-dark">添加珠子</button>
-              </td>
-              <td>
-
-              </td>
-            </tr>
           </tbody>
         </table>
+        <div>
+          <button type="button" className="btn btn-primary" id="addDB">添加</button>
+          <button type="button" className="btn btn-secondary">修改</button>
+          <button type="button" className="btn btn-success">查询</button>
+          <button type="button" className="btn btn-danger">删除</button>
+        </div>
       </div>
     )
   }
---
+++ src/list.js
@@ -0,0 +1,23 @@
+export default {
+  R6: [{
+    id: 1,
+    name: '耐毒'
+  }, {
+    id: 2,
+    name: '耐麻'
+  }, {
+    id: 3,
+    name: '耐火'
+  }],
+  R7: [{
+    id: 4,
+    name: '治愈',
+  }],
+  R8: [{
+    id: 5,
+    name: '匠'
+  }, {
+    id: 6,
+    name: '麻痹瓶'
+  }]
+}
