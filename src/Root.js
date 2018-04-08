b844e92f5c1f98b92d422651cc2d03c347cd905d
姜峰 <jiang.f@Ctrip.com>
2018-04-08 17:28:53 +0800

    'modify'

--- src/Root.js
+++ src/Root.js
@@ -1,4 +1,5 @@
 // https://github.com/jfengsky/javascript/blob/master/indexedDB/demo.js
+// indexDB 参考 http://javascript.ruanyifeng.com/bom/indexeddb.html
 
 import React, { Component } from 'react'
 
@@ -6,165 +7,51 @@ export default class Root extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      dblist: []
+      db: null
     }
   }
 
   componentDidMount() {
-    // let storage = window.localStorage
-    // let json = storage.getItem("alchemy")
-    // if (json) {
-    //   this.setState({
-    //     dblist: JSON.parse(json)
+
+    const request = indexedDB.open("test", 2)
+    let db
+
+    // 第一次打开数据库时，会先触发upgradeneeded事件，然后触发success事件。
+    request.onupgradeneeded = e => {
+      console.log("Upgrading...")
+    }
+
+    request.onsuccess = e => {
+      console.log("Success!")
+      db = request.result
+      console.log(db)
+      this.setState({
+        db
+      })
+    }
+
+    request.onerror = e => {
+      console.log("Error")
+      console.log(e);
+    }
+
+    // request.onupgradeneeded = function () {
+    //   console.log('upgrade')
+    //   let db = request.result
+    //   let store = db.createObjectStore("books", {
+    //     keyPath: "isbn"
     //   })
+    //   let titleIndex = store.createIndex("by_title", "title", {
+    //     unique: true
+    //   })
+    //   let authorIndex = store.createIndex("by_author", "author")
     // }
 
-    /*
-
-
-
-
-
-    var idbSupported = false, // 是否支持indexedDB 默认为不支持
-      db;
-    var indexedDB = window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB || window.msIndexedDB;
-
-    var customerData = [{
-      ssn: '444-44-444',
-      name: 'bill',
-      age: 25,
-      email: 'bill@mail.com'
-    }, {
-      ssn: '555-55-5555',
-      name: 'terry',
-      age: 32,
-      email: 'terry@gmail.com'
-    }];
-
-    document.addEventListener('DOMContentLoaded', function () {
-      if ('indexedDB' in window) {
-        idbSupported = true;
-      }
-
-      if (idbSupported) {
-        $('#addIndexedDB').click(function () {
-          var dbname = $('#dbname').val();
-        });
-        var request = indexedDB.open("library");
-        request.onupgradeneeded = function () {
-          console.log('upgrade');
-          var db = request.result;
-          var store = db.createObjectStore("books", {
-            keyPath: "isbn"
-          });
-          var titleIndex = store.createIndex("by_title", "title", {
-            unique: true
-          });
-          var authorIndex = store.createIndex("by_author", "author");
-
-        }
-
-        request.onsuccess = function (ev) {
-          console.log('success');
-          db = request.result;
-
-          document.getElementById('addDB').addEventListener('click', function () {
-            console.log('add store');
-            var tx = db.transaction("books", "readwrite");
-            var store = tx.objectStore("books");
-            var temp_title = $('#tit').val();
-            var temp_auth = $('#auth').val();
-            var temp_isbn = $('#isbn').val() - 0;
-            // 添加 store
-            store.put({
-              title: temp_title,
-              author: temp_auth,
-              isbn: temp_isbn
-            });
-            tx.complete = function () {
-              console.log('push complete');
-            }
-          });
-
-          // del store
-          $('#delstore').click(function () {
-            console.log('del store');
-            var tx = db.transaction("books", "readwrite");
-            var store = tx.objectStore("books");
-            var del_key = parseInt($('#delkey').val());
-            console.log(del_key);
-            var delstore = store.delete(del_key);
-            delstore.onsuccess = function () {
-              console.log('has delete');
-            }
-          });
-
-          // get store
-          $('#getkeybutton').click(function () {
-            console.log('get key');
-            var tx = db.transaction("books", "readwrite");
-            var store = tx.objectStore("books");
-            var get_key = parseInt($('#getkey').val());
-            var getstore = store.get(get_key);
-            console.log(getstore);
-            getstore.onsuccess = function (ev) {
-              console.log(this.result);
-            }
-          });
-
-
-          // del DB
-          $('#delDB').click(function () {
-            console.log('del DB');
-            db.delete("library");
-          });
-
-          $('#getCursor').click(function () {
-            var tx = db.transaction("books", "readonly");
-            var store = tx.objectStore("books");
-            var index = store.index("by_author");
-
-            var request2 = index.openCursor(IDBKeyRange.only("auth2"));
-
-            request2.onsuccess = function () {
-              var cursor = request2.result;
-              if (cursor) {
-                // Called for each matching record.
-                console.log(cursor.value);
-                // console.log(cursor.value.isbn, cursor.value.title, cursor.value.author);
-                cursor.continue();
-              } else {
-                // No more matching records.
-                console.log('get null');
-              }
-            };
-
-
-
-          });
-          db.close();
-          console.log('close')
-        }
-
-      }
-
-
-
-    }, false);
-
-
-    */
-
-
-
-
-
-
-
-
-
-
-
+    // request.onsuccess = ev => {
+    //   this.setState({
+    //     db: request.result
+    //   })
+    // }
   }
 
   render() {
@@ -256,10 +143,10 @@ export default class Root extends Component {
           <input type="text" id="dbname" />
           <input type="button" value="add DB" id="addIndexedDB" />
           <br />
-          <label htmlFor="">title:<input type="text" id="tit" /></label>
-          <label htmlFor="">author:<input type="text" id="auth" /></label>
-          <label htmlFor="">isbn:<input type="text" id="isbn" /></label>
-          <input type="button" value="add store" id="addDB" />
+          <label htmlFor="">title:<input type="text" ref="title" /></label>
+          <label htmlFor="">author:<input type="text" ref="author" /></label>
+          <label htmlFor="">isbn:<input type="text" ref="isbn" /></label>
+          <input type="button" value="add store" onClick={this.addDaba} />
           <br />
           <input type="text" name="" id="delkey" />
           <input type="button" value="del store" id="delstore" />
@@ -279,6 +166,34 @@ export default class Root extends Component {
 
   createLayer = e => {
     let container = document.createElement('div')
+  }
+
+  addDaba = e => {
+    console.log('add store')
+    let db = this.state.db
+    let {
+      title,
+      author,
+      isbn
+    } = this.refs
+    if (db) {
+
+      // 检查有没有数据仓库，没有就创建一个
+      if(!db.objectStoreNames.contains("firstOS")) {
+        db.createObjectStore("firstOS", { keyPath: "email" })
+      }
+
+      let tx = db.transaction("books", "readwrite")
+      let store = tx.objectStore("books")
+      store.put({
+        title: title.value,
+        author: author.value,
+        isbn: isbn.value
+      })
+      tx.complete = function () {
+        console.log('push complete');
+      }
+    }
   }
 
 }
